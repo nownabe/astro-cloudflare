3286fe85f06735bf71fb10d3ced827f093e278a5
nownabe <nownabe@gmail.com>
2024-11-04 22:35:58 +0900

    disable cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  adapter: cloudflare({}),
+  // adapter: cloudflare({}),
 });
